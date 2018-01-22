2be05ebeeadc615c92b1263f5b8f45347a894702
Walter Nunes Filho <jnunes.walter@gmail.com>
2018-01-21 23:18:04 -0200

    added li categories

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -16,10 +16,10 @@ class Categories extends Component {
       <div className="col-sm-3 sidenav">
         <h4>Categories</h4>
         <ul className="nav nav-pills nav-stacked">
-          <li className="active"><a href="#section1">Home</a></li>
-          <li><a href="#section2">Friends</a></li>
-          <li><a href="#section3">Family</a></li>
-          <li><a href="#section3">Photos</a></li>
+          {categories.length > 1 && categories.map((category, index) => (
+              <li key={index}><a href="#">{category.name}</a>
+              </li>
+          ))}
         </ul>
       </div>
     )
